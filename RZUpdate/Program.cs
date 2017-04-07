efc148011516b5d5a40c340e3c3e53404a6e7afd
Roger Zander <roger@zander.ch>
2017-04-07 20:19:29 +0200

    RZUpdate initial upload

---
+++ RZUpdate/Program.cs
@@ -0,0 +1,206 @@
+﻿//
+// RZUpdate (C) 2017 by Roger Zander
+// This Tool is under MS-Pl License (http://ruckzuck.codeplex.com/license)
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.IO;
+using RuckZuck_WCF;
+
+namespace RZUpdate
+{
+    class Program
+    {
+        public static RZScan oScan;
+        public static RZUpdater oUpdate;
+        public static bool bRunning = true;
+
+        static void Main(string[] args)
+        {
+            //Get Proxy from IE
+            WebRequest.DefaultWebProxy = WebRequest.GetSystemWebProxy();
+
+            List<string> lArgs = args.ToList();
+            if (lArgs.Contains("-?") | lArgs.Contains("/?") | lArgs.Count < 1)
+            {
+                Console.WriteLine("RuckZuck Update Tool (c) 2017 by Roger Zander");
+                Console.WriteLine("Usage:");
+                Console.WriteLine("Check and Update an existing Software : RZUpdate.exe \"<ProductName>\" \"<ProductVersion>\"");
+                Console.WriteLine("Install a Software from Shortname : RZUpdate.exe \"<Shortname>\"[;\"<Shortname2>\"]");
+                Console.WriteLine("Install a Software from XML-File: RZUpdate.exe \"<RZXML File.xml>\"");
+                Console.WriteLine("Update all installed Software-Versions: RZUpdate.exe /Update");
+                Console.WriteLine("");
+                return;
+            }
+
+            RZRestAPI.sURL = Properties.Settings.Default.WebService;
+
+            if (lArgs.Count == 1)
+            {
+                if (File.Exists(lArgs[0]))
+                {
+                    RZUpdater oRZSW = new RZUpdater(lArgs[0]);
+
+                    Console.WriteLine(oRZSW.SoftwareUpdate.SW.ProductName + " " + oRZSW.SoftwareUpdate.SW.ProductVersion + " :");
+                    Console.Write("Downloading...");
+                    if (oRZSW.SoftwareUpdate.Download().Result)
+                    {
+                        Console.WriteLine("... done.");
+                        Console.Write("Installing...");
+                        if (oRZSW.SoftwareUpdate.Install().Result)
+                        {
+                            Console.WriteLine("... done.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("... Error. The installation failed.");
+                        }
+                    }
+                }
+                else
+                {
+                    if (lArgs[0].ToLower() == "/update" | lArgs[0].ToLower() == "-update")
+                    {
+                        oUpdate = new RZUpdater();
+                        oScan = new RZScan(true, true);
+                        Console.Write("Detecting updates...");
+                        oScan.OnUpdScanCompleted += OScan_OnUpdScanCompleted;
+
+                        while (bRunning)
+                        {
+                            System.Threading.Thread.Sleep(100);
+                        }
+                    }
+                    else
+                    {
+                        foreach (string sArg in lArgs[0].Split(';'))
+                        {
+                            try
+                            {
+                                RZUpdater oRZSW = new RZUpdater();
+                                oRZSW.SoftwareUpdate = new SWUpdate(sArg.Trim('"').Trim());
+
+                                if (string.IsNullOrEmpty(oRZSW.SoftwareUpdate.SW.ProductName))
+                                {
+                                    Console.WriteLine("'" + sArg + "' is NOT available in RuckZuck...!");
+                                    continue;
+                                }
+
+
+                                Console.WriteLine(oRZSW.SoftwareUpdate.SW.Manufacturer + " " + oRZSW.SoftwareUpdate.SW.ProductName + " " + oRZSW.SoftwareUpdate.SW.ProductVersion);
+                                Console.Write("Downloading...");
+                                foreach (string sPreReq in oRZSW.SoftwareUpdate.SW.PreRequisites)
+                                {
+                                    RZUpdater oRZSWPreReq = new RZUpdater();
+                                    oRZSWPreReq.SoftwareUpdate = new SWUpdate(sPreReq);
+                                    Console.WriteLine();
+                                    Console.Write("\tDownloading dependencies (" + oRZSWPreReq.SoftwareUpdate.SW.Shortname + ")...");
+                                    if (oRZSWPreReq.SoftwareUpdate.Download().Result)
+                                    {
+                                        Console.WriteLine("... done.");
+                                        Console.Write("\tInstalling dependencies (" + oRZSWPreReq.SoftwareUpdate.SW.Shortname + ")...");
+                                        if (oRZSWPreReq.SoftwareUpdate.Install().Result)
+                                        {
+                                            Console.WriteLine("... done.");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("... Error. The installation failed.");
+                                        }
+                                    }
+
+                                }
+                                if (oRZSW.SoftwareUpdate.Download().Result)
+                                {
+                                    Console.WriteLine("... done.");
+                                    Console.Write("Installing...");
+                                    if (oRZSW.SoftwareUpdate.Install().Result)
+                                    {
+                                        Console.WriteLine("... done.");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("... Error. The installation failed.");
+                                    }
+                                }
+                            }
+                            catch(Exception ex)
+                            {
+                                Console.WriteLine("Error: " + ex.Message);
+                            }
+                        }
+
+                    }
+                }
+            }
+            if (lArgs.Count == 2)
+            {
+                RZUpdater oRZUpdate = new RZUpdater();
+                var oUpdate = oRZUpdate.CheckForUpdate(lArgs[0], lArgs[1]);
+                if (oUpdate != null)
+                {
+                    Console.WriteLine("New Version: " + oUpdate.SW.ProductVersion);
+                    Console.Write("Downloading...");
+
+                    if (oUpdate.Download().Result)
+                    {
+                        Console.WriteLine("... done.");
+                        Console.Write("Installing...");
+                        if (oUpdate.Install(false).Result)
+                        {
+                            Console.WriteLine("... done.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("... Error. The update installation failed.");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No Update found...");
+                }
+            }
+
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        private static void OScan_OnUpdScanCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("... done.");
+            Console.WriteLine(oScan.NewSoftwareVersions.Count + " updates found.");
+
+            foreach (RuckZuck_WCF.AddSoftware oSW in oScan.NewSoftwareVersions)
+            {
+                try
+                {
+                    Console.WriteLine(oSW.Manufacturer + " " + oSW.ProductName + " new Version: " + oSW.ProductVersion);
+                    oUpdate.SoftwareUpdate.SW = oSW;
+                    oUpdate.SoftwareUpdate.GetInstallType();
+                    Console.Write("Downloading...");
+                    if (oUpdate.SoftwareUpdate.Download().Result)
+                    {
+                        Console.WriteLine("... done.");
+                        Console.Write("Installing...");
+                        if (oUpdate.SoftwareUpdate.Install(false).Result)
+                        {
+                            Console.WriteLine("... done.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("... Error. The update installation failed.");
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("E131:" + ex.Message);
+                }
+            }
+            bRunning = false;       
+        }
+    }
+}
---
+++ RZUpdate/RZRestAPI.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Linq;
+
+namespace RuckZuck_WCF
+{
+    public static class RZRestAPI
+    {
+        public static string sURL = "https://ruckzuck.azurewebsites.net/wcf/RZService.svc";
+        //internal static string sURL = "http://localhost:7727/RZService.svc";
+
+        public static string Token;
+        public static string GetAuthToken(string Username, string Password)
+        {
+            try
+            {
+                WebClient oClient = new WebClient();
+                oClient.Headers.Add("Username:" + Username);
+                oClient.Headers.Add("Password:" + Password);
+                oClient.Headers.Add("Content-Type:application/json");
+                string sResult = oClient.DownloadString(sURL + "/rest/AuthenticateUser");
+                Token = sResult.Replace("\"", "");
+                return Token;
+            }
+            catch { }
+
+            return "";
+
+        }
+
+        public static List<GetSoftware> SWGet(string Shortname)
+        {
+            WebClient oClient = new WebClient();
+            oClient.Headers.Add("AuthenticatedToken:" + Token);
+            oClient.Headers.Add("Content-Type:application/xml");
+            string sResult = oClient.DownloadString(sURL + "/rest/SWGetShort?name=" + WebUtility.UrlEncode(Shortname));
+
+            return DataContractDeSerializeObject<List<GetSoftware>>(sResult);
+        }
+
+        public static List<GetSoftware> SWGet(string PackageName, string PackageVersion)
+        {
+            WebClient oClient = new WebClient();
+            oClient.Headers.Add("AuthenticatedToken:" + Token);
+            oClient.Headers.Add("Content-Type:application/xml");
+            string sResult = oClient.DownloadString(sURL + "/rest/SWGet?name=" + WebUtility.UrlEncode(PackageName) + "&ver=" + PackageVersion);
+
+            return DataContractDeSerializeObject<List<GetSoftware>>(sResult);
+        }
+
+        public static List<GetSoftware> SWResults(string Searchstring)
+        {
+            try
+            {
+                WebClient oClient = new WebClient();
+                oClient.Headers.Add("AuthenticatedToken:" + Token);
+                oClient.Headers.Add("Content-Type:application/xml");
+                string sResult = oClient.DownloadString(sURL + "/rest/SWResults?search=" + Searchstring);
+
+                return DataContractDeSerializeObject<List<GetSoftware>>(sResult);
+            }
+            catch { }
+
+            return new List<GetSoftware>();
+        }
+
+        public static async Task Feedback(string productName, string productVersion, string manufacturer, string working, string userKey, string feedback)
+        {
+            if (!string.IsNullOrEmpty(feedback))
+            {
+                await Task.Run(() =>
+                {
+                    try
+                    {
+                        WebClient oClient = new WebClient();
+                        oClient.Headers.Add("AuthenticatedToken:" + Token);
+                        oClient.Headers.Add("Content-Type:application/xml");
+                        string sResult = oClient.DownloadString(sURL + "/rest/Feedback?name=" + WebUtility.UrlEncode(productName) + "&ver=" + WebUtility.UrlEncode(productVersion) + "&man=" + WebUtility.UrlEncode(manufacturer) + "&ok=" + working + "&user=" + WebUtility.UrlEncode(userKey) + "&text=" + WebUtility.UrlEncode(feedback));
+                    }
+                    catch { }
+                });
+            }
+        }
+
+        public static List<AddSoftware> GetSWDefinitions(string productName, string productVersion, string manufacturer)
+        {
+            WebClient oClient = new WebClient();
+            oClient.Headers.Add("AuthenticatedToken:" + Token);
+            oClient.Headers.Add("Content-Type:application/xml");
+            string sResult = oClient.DownloadString(sURL + "/rest/GetSWDefinition?name=" + WebUtility.UrlEncode(productName) + "&ver=" + WebUtility.UrlEncode(productVersion) + "&man=" + WebUtility.UrlEncode(manufacturer));
+
+            return DataContractDeSerializeObject<List<AddSoftware>>(sResult);
+        }
+
+        public static List<AddSoftware> CheckForUpdate(List<AddSoftware> lSoftware)
+        {
+            try
+            {
+                string sData = DataContractSerializeObject<List<AddSoftware>>(lSoftware);
+
+                byte[] bytes = Encoding.UTF8.GetBytes(sData);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(sURL + "/rest/CheckForUpdateXml");
+                request.Method = "POST";
+                request.ContentLength = bytes.Length;
+                request.Headers.Add("AuthenticatedToken:" + Token);
+                request.ContentType = "application/xml";
+
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    requestStream.Write(bytes, 0, bytes.Length);
+                }
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream resStream = response.GetResponseStream();
+                StreamReader rdStreamRdr = new StreamReader(resStream);
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    string message = String.Format("POST Failed. Received HTTP {0}",
+                                response.StatusCode);
+                    throw new ApplicationException(message);
+                }
+                else
+                {
+                    string message = rdStreamRdr.ReadToEnd();
+                    message.ToString();
+                    var oResult = DataContractDeSerializeObject<List<AddSoftware>>(message);
+                    return oResult;
+                }
+            }
+            catch { }
+
+            return new List<AddSoftware>();
+        }
+
+        public static bool UploadSWEntry(AddSoftware lSoftware)
+        {
+            try
+            {
+                string sData = DataContractSerializeObject<AddSoftware>(lSoftware);
+
+                byte[] bytes = Encoding.UTF8.GetBytes(sData);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(sURL + "/rest/UploadSWEntry");
+                request.Method = "POST";
+                request.ContentLength = bytes.Length;
+                request.Headers.Add("AuthenticatedToken:" + Token);
+                request.ContentType = "application/xml";
+
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    requestStream.Write(bytes, 0, bytes.Length);
+                }
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream resStream = response.GetResponseStream();
+                StreamReader rdStreamRdr = new StreamReader(resStream);
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    string message = String.Format("POST Failed. Received HTTP {0}",
+                                response.StatusCode);
+                    throw new ApplicationException(message);
+                }
+                else
+                {
+                    string message = rdStreamRdr.ReadToEnd();
+                    var oResult = DataContractDeSerializeObject<bool>(message);
+                    return oResult;
+                }
+            }
+            catch { }
+
+            return false;
+        }
+
+        public static string DataContractSerializeObject<T>(T objectToSerialize)
+        {
+            using (MemoryStream memStm = new MemoryStream())
+            {
+                var serializer = new DataContractSerializer(typeof(T));
+                serializer.WriteObject(memStm, objectToSerialize);
+
+                memStm.Seek(0, SeekOrigin.Begin);
+
+                using (var streamReader = new StreamReader(memStm))
+                {
+                    string result = streamReader.ReadToEnd();
+                    return result;
+                }
+            }
+        }
+
+        public static T DataContractDeSerializeObject<T>(string objectToDeSerialize)
+        {
+            // string sHeader = "<?xml version=\"1.0\" encoding=\"UTF-16LE\" ?>";
+            var serializer = new DataContractSerializer(typeof(T));
+
+            T response;
+
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(objectToDeSerialize)))
+            {
+                response = (T)serializer.ReadObject(ms);
+            }
+
+            return response;
+        }
+
+        public static async void TrackDownloads(string contentID)
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    WebClient oClient = new WebClient();
+                    oClient.Headers.Add("AuthenticatedToken:" + Token);
+                    oClient.Headers.Add("Content-Type:application/xml");
+                    string sResult = oClient.DownloadString(sURL + "/rest/TrackDownloads/" + WebUtility.UrlEncode(contentID));
+                }
+                catch { }
+            });
+        }
+
+        public static List<string> GetCategories(List<GetSoftware> oSWList)
+        {
+            List<string> lResult = new List<string>();
+
+            foreach (GetSoftware oSW in oSWList)
+            {
+                lResult.AddRange((oSW.Categories ?? new List<string>()).ToArray());
+            }
+
+            return lResult.Distinct().OrderBy(t => t).ToList();
+        }
+    }
+
+    public class GetSoftware
+    {
+        public string ProductName { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Description { get; set; }
+
+        public string Shortname { get; set; }
+
+        public string ProductURL { get; set; }
+
+        public string ProductVersion { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public Int32? Quality { get; set; }
+
+        public Int32? Downloads { get; set; }
+
+        public List<string> Categories { get; set; }
+
+        public long IconId { get; set; }
+
+        public bool isInstalled { get; set; }
+
+        public string XMLFile { get; set; }
+
+        public string IconFile { get; set; }
+
+        public string IconURL
+        {
+            get
+            {
+                if (IconId > 0)
+                {
+                    return RZRestAPI.sURL.Replace("RZService.svc", "GetIcon.ashx?Id=") + IconId.ToString();
+                }
+                else
+                {
+                    return "File://" + IconFile;
+                }
+                //return "https://ruckzuck.azurewebsites.net/wcf/GetIcon.ashx?Id=" + IconId.ToString();
+            }
+        }
+
+    }
+
+    public class AddSoftware
+    {
+        public string ProductName { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Description { get; set; }
+
+        public string Shortname { get; set; }
+
+        public string ProductURL { get; set; }
+
+        public string ProductVersion { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public string MSIProductID { get; set; }
+
+        public string Architecture { get; set; }
+
+        public string PSUninstall { get; set; }
+
+        public string PSDetection { get; set; }
+
+        public string PSInstall { get; set; }
+
+        public string PSPreReq { get; set; }
+
+        public string PSPreInstall { get; set; }
+
+        public string PSPostInstall { get; set; }
+
+        public string ContentID { get; set; }
+
+        public List<contentFiles> Files { get; set; }
+
+        public string Author { get; set; }
+
+        public string Category { get; set; }
+
+        public string[] PreRequisites { get; set; }
+    }
+
+    public class contentFiles
+    {
+        public string URL { get; set; }
+        public string FileName { get; set; }
+        public string FileHash { get; set; }
+        public string HashType { get; set; }
+    }
+
+    public class DLTask
+    {
+        public string ProductName { get; set; }
+
+        public string ProductVersion { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Shortname { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public bool AutoInstall { get; set; }
+
+        public bool Installed { get; set; }
+
+        public bool UnInstalled { get; set; }
+
+        public bool Downloading { get; set; }
+
+        public bool Installing { get; set; }
+
+        public bool Error { get; set; }
+
+        public bool WaitingForDependency { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public string Status
+        {
+            get
+            {
+                if (Installing & !Error)
+                    return "Installing";
+                if (Downloading & !Error)
+                    return "Downloading";
+                if (Installed & !Error)
+                    return "Installed";
+                if (UnInstalled & !Error)
+                    return "Uninstalled...";
+                if (WaitingForDependency)
+                    return "Waiting for dependencies";
+                if (PercentDownloaded == 100 & !Error)
+                    return "Downloaded";
+                if (Error)
+                    return ErrorMessage;
+
+                return "Waiting";
+            }
+        }
+
+        public long DownloadedBytes { get; set; }
+
+        public long TotalBytes { get; set; }
+
+        public int PercentDownloaded { get; set; }
+
+        public List<contentFiles> Files { get; set; }
+
+        //public Task task { get; set; }
+
+        public RZUpdate.SWUpdate SWUpd { get; set; }
+    }
+
+    public class DLStatus
+    {
+        public string Filename { get; set; }
+
+        public string URL { get; set; }
+
+        public int PercentDownloaded { get; set; }
+
+        public long DownloadedBytes { get; set; }
+
+        public long TotalBytes { get; set; }
+    }
+}
