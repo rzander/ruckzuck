e1c4a8a4c58ffc3c5948eadc1254888b028eda2d
Roger Zander <roger@zander.ch>
2018-02-14 17:53:24 +0100

    Trace output

--- RZUpdate/RZScan.cs
+++ RZUpdate/RZScan.cs
@@ -254,7 +254,9 @@ namespace RZUpdate
         {
             if (bCheckUpdates)
             {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                 CheckUpdates(null);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             }
         }
 
--- RZUpdateTest/UnitTest1.cs
+++ RZUpdateTest/UnitTest1.cs
@@ -7,15 +7,24 @@ namespace RZUpdateTest
     [TestClass]
     public class UnitTest1
     {
+        private TestContext tr;
+
         [TestMethod]
         public void TestMethodDownloadUpdate()
         {
+            tr.WriteLine("Check Update for SCCMCliCtr 1.0.3.9...");
             RZUpdater oRZSW = new RZUpdater();
             var oUpdate = oRZSW.CheckForUpdate("Client Center for Configuration Manager", "1.0.3.9", "Zander Tools");
-            
+            tr.WriteLine("found Version:" + oRZSW.SoftwareUpdate.SW.ProductVersion);
             Assert.IsNotNull(oUpdate, "Update detected successfully.");
             Assert.IsNotNull(oUpdate.SW.ContentID, "Update conatins ContentID");
-            Assert.IsTrue(oUpdate.Download().Result, "Files downloaded successfully.");
+            tr.WriteLine("Downloading files..");
+            bool bDLResult = oUpdate.Download().Result;
+            Assert.IsTrue(bDLResult, "Files downloaded successfully.");
+            if (bDLResult)
+                tr.WriteLine("..done.");
+            else
+                tr.WriteLine("download failed !!");
         }
     }
 }
