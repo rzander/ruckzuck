53cbcba7cfbe16bfb5012181736297d52739b282
Roger Zander <roger@zander.ch>
2018-02-14 17:17:07 +0100

    New: TestUnit for RZUpdate
    test update detection and download.

---
+++ RZUpdateTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RZUpdate;
+
+namespace RZUpdateTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodDownloadUpdate()
+        {
+            RZUpdater oRZSW = new RZUpdater();
+            var oUpdate = oRZSW.CheckForUpdate("Client Center for Configuration Manager", "1.0.3.9", "Zander Tools");
+            
+            Assert.IsNotNull(oUpdate, "Update detected successfully.");
+            Assert.IsNotNull(oUpdate.SW.ContentID, "Update conatins ContentID");
+            Assert.IsTrue(oUpdate.Download().Result, "Files downloaded successfully.");
+        }
+    }
+}
