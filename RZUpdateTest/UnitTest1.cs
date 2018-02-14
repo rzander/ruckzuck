df4ad7b755685a5ac19d2326e35e992b16a0b9c4
Roger Zander <roger@zander.ch>
2018-02-14 18:02:41 +0100

    fix trace

--- RZUpdateTest/UnitTest1.cs
+++ RZUpdateTest/UnitTest1.cs
@@ -7,24 +7,22 @@ namespace RZUpdateTest
     [TestClass]
     public class UnitTest1
     {
-        private TestContext tr;
-
         [TestMethod]
         public void TestMethodDownloadUpdate()
         {
-            tr.WriteLine("Check Update for SCCMCliCtr 1.0.3.9...");
+            Console.WriteLine("Check Update for SCCMCliCtr 1.0.3.9...");
             RZUpdater oRZSW = new RZUpdater();
             var oUpdate = oRZSW.CheckForUpdate("Client Center for Configuration Manager", "1.0.3.9", "Zander Tools");
-            tr.WriteLine("found Version:" + oRZSW.SoftwareUpdate.SW.ProductVersion);
+            Console.WriteLine("found Version:" + oRZSW.SoftwareUpdate.SW.ProductVersion);
             Assert.IsNotNull(oUpdate, "Update detected successfully.");
             Assert.IsNotNull(oUpdate.SW.ContentID, "Update conatins ContentID");
-            tr.WriteLine("Downloading files..");
+            Console.Write("Downloading files..");
             bool bDLResult = oUpdate.Download().Result;
             Assert.IsTrue(bDLResult, "Files downloaded successfully.");
             if (bDLResult)
-                tr.WriteLine("..done.");
+                Console.WriteLine("..done.");
             else
-                tr.WriteLine("download failed !!");
+                Console.WriteLine("download failed !!");
         }
     }
 }
