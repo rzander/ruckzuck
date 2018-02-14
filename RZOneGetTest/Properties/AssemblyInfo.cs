ca17fee946514eeca16458b4c31369ce7266a214
Roger Zander <roger@zander.ch>
2018-02-14 18:40:44 +0100

    New test unit

---
+++ RZOneGetTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RZOneGetTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zander Tools")]
+[assembly: AssemblyProduct("RZOneGetTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018 by Roger Zander")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dde262cf-0e7b-4826-aa99-dac20459aa49")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RZOneGetTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PackageManagement;
+
+namespace RZOneGetTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FindPackage()
+        {
+            var pkg = new PackageProvider();
+            var oRes = pkg.RunPS("Find-Package -Provider RuckZuck -Name sccmclictr");
+            Assert.AreEqual(pkg.PackageProviderName, "RuckZuck");
+            Assert.IsNotNull(oRes);
+            if(oRes != null)
+            {
+                Console.Write("Latest Version of 'sccmclictr': ");
+                string sVersion = (((System.Management.Automation.PSProperty)oRes[0].Properties["Version"]).Value as string) ?? "";
+                Console.WriteLine(sVersion);
+                Assert.AreNotEqual(sVersion, "");
+            }
+        }
+    }
+}
