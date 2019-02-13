fc4d724115f96af87d0f8d9e18775d9a71c4ecec
Roger Zander <roger@zander.ch>
2019-02-13 21:51:34 +0100

    New: Dowload Controller for backward compatibility

---
+++ RZ.Server/RZ.Server/Controllers/DLController.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RZ.Server.Controllers
+{
+    [Microsoft.AspNetCore.Authorization.AllowAnonymous]
+    public class DLController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        [Route("DL/{filename}")]
+        public IActionResult DL(string filename)
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "DL", filename), "application/octet-stream");
+        }
+    }
+}
