0e4827294f3beec38cde88f6dcd33fa3be7a7e9a
Roger Zander <roger@zander.ch>
2019-11-01 20:50:47 +0100

    Fix: E131 Error on RZUpdate of no RZUpdate.exe.config file exists
    https://github.com/rzander/ruckzuck/issues/63

--- RZUpdate/Properties/Settings.Designer.cs
+++ RZUpdate/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace RZUpdate.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,6 +25,9 @@ namespace RZUpdate.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
+            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
+            "tring>0</string>\r\n</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection Excludes {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["Excludes"]));
