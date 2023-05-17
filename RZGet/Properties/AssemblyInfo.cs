fc3e70e8b28df7314e2673a9aa20c64eca0a9b43
Roger Zander <roger@zander.ch>
2023-05-17 18:03:11 +0200

    Update References

--- RZGet/Properties/AssemblyInfo.cs
+++ RZGet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zander Tools")]
 [assembly: AssemblyProduct("RZGet")]
-[assembly: AssemblyCopyright("Copyright ©  2022 by Roger Zander")]
+[assembly: AssemblyCopyright("Copyright ©  2023 by Roger Zander")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.2.*")]
-[assembly: AssemblyFileVersion("1.7.2.5")]
+[assembly: AssemblyVersion("1.7.3.*")]
+[assembly: AssemblyFileVersion("1.7.3.1")]
